f43532ef45bb8563dfceb2e8b39b38a0e71d5db9
Alfindi Abdillah Farizky <kevinrhesa@gmail.com>
2020-11-04 17:19:44 +0700

    Adapter Updates

---
+++ app/src/main/java/com/example/firebaseapps/CourseData.java
@@ -0,0 +1,94 @@
+package com.example.firebaseapps;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.firebaseapps.Model.Course;
+import com.example.firebaseapps.adapter.CourseAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class CourseData extends AppCompatActivity {
+
+    Toolbar courseDataToolbar;
+    RecyclerView courseDataRV;
+
+    DatabaseReference dbCourse;
+
+    ArrayList<Course> listCourse = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_data);
+
+        setSupportActionBar(courseDataToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        dbCourse = FirebaseDatabase.getInstance().getReference("course");
+        fetchCourseData();
+
+    }
+
+    private void fetchCourseData() {
+        dbCourse.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                listCourse.clear();
+                courseDataRV.setAdapter(null);
+                for(DataSnapshot childSnapshot : snapshot.getChildren()){
+                    Course course = childSnapshot.getValue(Course.class);
+                    listCourse.add(course);
+                }
+                showCourseData(listCourse);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void showCourseData(ArrayList<Course> list) {
+        courseDataRV.setLayoutManager(new LinearLayoutManager(CourseData.this));
+        CourseAdapter courseAdapter = new CourseAdapter(CourseData.this);
+        courseAdapter.setListCourse(list);
+        courseDataRV.setAdapter(courseAdapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            Intent intent = new Intent(CourseData.this, AddCourse.class);
+            intent.putExtra("action", "add");
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(CourseData.this, AddCourse.class);
+        intent.putExtra("action", "add");
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+    }
+}
--- app/src/main/java/com/example/firebaseapps/MainActivity.java
+++ app/src/main/java/com/example/firebaseapps/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.firebaseapps;
 
-import android.os.Bundle;
-import android.view.View;
+        import android.os.Bundle;
+        import android.view.View;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+        import com.google.android.material.floatingactionbutton.FloatingActionButton;
+        import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
